14c7e0093dca6ce7c72b9f564d4a13becb3e3517
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-07-04 15:50:22 +0530

    folder structure change

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const connectDB = require('./config/db');
+const connectDB = require('../config/db');
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
