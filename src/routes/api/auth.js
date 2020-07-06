8c6e6c429bcdf5a674775834cb84e729c1cdad29
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-07-06 21:36:01 +0530

    login functionality

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../../middleware/auth');
 const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 // @route   GET api/auth
 // @desc    Retrieve registered user
@@ -15,5 +19,60 @@ router.get('/', auth, async (req, res) => {
     res.status(500).json('Internal Server Error');
   }
 });
+
+// @route   POST api/auth
+// @desc    login authentic user
+// @access  Public
+router.post(
+  '/',
+  [
+    check('email', 'Input a valid Email').isEmail(),
+    check('password', 'Input password').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      console.log('get user');
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credential' }] });
+      }
+
+      console.log('check password');
+      if (!(await bcrypt.compare(password, user.password))) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credential' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          console.log('jwt token');
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+);
 
 module.exports = router;
